20d7c8217776d47a5a6a9832d38ffec47d295d2a
TomonoriIshikawa <ishikawa@aidma-hd.jp>
2019-08-28 10:49:36 +0900

    Create word2vec.py

---
+++ word2vec.py
@@ -0,0 +1,7 @@
+from gensim.models import word2vec
+
+sentences = word2vec.Text8Corpus('./wiki_wakati.txt')
+
+model = word2vec.Word2Vec(sentences, size=200, min_count=20, window=15)
+
+model.save("./wiki.model")
